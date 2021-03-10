831e816d715e6e6804f4105e4bc916a5b486fffb
yusalie <71071393+yusalie@users.noreply.github.com>
2021-03-10 13:32:10 +0200

    Update payment.js

--- assets/script/payment.js
+++ assets/script/payment.js
@@ -3,11 +3,7 @@
 // }
 
 function amountItems() {
-  cart = localStorage.getItem("cart");
-  console.log(cart);
-  length = cart.length;
-  console.log(length);
-  total = length * 8;
+  total = JSON.parse(localStorage.getItem("cart")).length * 8;
   console.log(total);
   document.getElementById("amount-pay").innerHTML = "$" + total;
 }
