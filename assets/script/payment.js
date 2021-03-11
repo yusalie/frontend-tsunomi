2b2b4130e89c28fe8e4423c9d653bb6fea30f082
yusalie <71071393+yusalie@users.noreply.github.com>
2021-03-11 08:59:03 +0200

    Add files via upload

--- assets/script/payment.js
+++ assets/script/payment.js
@@ -1,9 +1,14 @@
-// function checkout() {
-//   localStorage.clear();
-// }
-
 function amountItems() {
   total = JSON.parse(localStorage.getItem("cart")).length * 8;
   console.log(total);
   document.getElementById("amount-pay").innerHTML = "$" + total;
+}
+
+function pay() {
+  alert("thanks for shopping with us");
+  window.location.href = "./index.html";
+}
+
+function contin() {
+  window.location.href = "./index.html";
 }
